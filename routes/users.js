560dbb2899c6c47179c25c691ffc38325d7ce51f
Yaw Sarfo <yawsarfo2019@gmail.com>
2023-05-22 14:13:21 +0000

    Added a login functionality

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,10 @@ router.get('/signup', function(req, res, next) {
   res.render('register');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 
 router.post('/signup',async (req,res,next)=>{
   const { email ,password, password1 } = req.body;
@@ -68,5 +72,39 @@ const newuser= await new User({ ...req.body, password:hashPassword}).save();
  return res.status(200).json({ message: 'User successfully created' });
 });
 
+router.post("/login", async (req, res) => {
+  // Validate the submitted data
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Please enter all required fields");
+  }
+
+  // Find the user by email
+  const user = await User.findOne({ email });
+
+  // Check if the user exists
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+
+  // Compare the submitted password to the hashed password in the database
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  // If the passwords don't match, return an error
+  if (!isPasswordValid) {
+    return res.status(401).send("Invalid email or password");
+  }
+
+  // Create a JWT and set it as a cookie
+  const token = jwt.sign({ email: user.email }, process.env.TokenSecret);
+  res.cookie("token", token, { httpOnly: true });
+
+  if (token){
+    return res.status(200).json({ message: 'successive login'});
+  }
+
+ 
+});
 
 module.exports = router;
