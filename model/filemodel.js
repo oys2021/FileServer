6860690285c54f4dfe17571a3bcc7450d02386cf
Yaw Sarfo <yawsarfo2019@gmail.com>
2023-05-26 12:38:19 +0000

    Adde the file model

---
+++ model/filemodel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var Schema= mongoose.Schema
+var bcrypt=require('bcrypt')
+const jwt = require("jsonwebtoken");
+
+var FileSchema=new Schema({
+  title: {type:String},
+  description: {type:String},
+  fileName: {type:String},
+  fileType: {type:String},
+  uploadDate: { type: Date, default: Date.now },
+  downloadCount: { type: Number, default: 0 },
+  emailCount: { type: Number, default: 0 },
+  ownerID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+
+});
+
