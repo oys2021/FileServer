755a302023105f71a7deaa41ffb91cc006e61cbe
Yaw Sarfo <yawsarfo2019@gmail.com>
2023-06-09 09:20:57 +0000

    added e a feed page that contains a list of files that can be downloaded

---
+++ routes/admin.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var jwt=require('jsonwebtoken')
+const multer = require('multer');
+var FileModel=require('../model/filemodel')
+/* GET home page. */
+
+// Set up multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Set the destination folder for file uploads
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname); // Generate unique filenames
+  }
+});
+
+// Set up multer upload
+const upload = multer({ storage: storage });
+
+
+router.get('/main',function(req, res, next) {
+  res.render('admin/dashboard');
+});
+
+router.get('/addfile',function(req, res, next) {
+  res.render('admin/addFile');
+});
+
+router.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const { filename, mimetype } = req.file;
+
+    const file = new FileModel({
+      title,
+      description,
+      fileName: filename,
+      fileType: mimetype,
+    });
+
+    await file.save();
+
+    res.status(200).json({ message: 'File uploaded successfully' });
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    res.status(500).json({ error: 'Failed to upload file' });
+  }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var FileModel=require('../model/filemodel')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.post('/searchFruits', async(req, res)=>{
+  var regex= new RegExp('noodles', 'i');
+  return FileModel.find({title: regex}, function(err,q){
+    return res.send(q);
+});
+});
+
+
 
 module.exports = router;
