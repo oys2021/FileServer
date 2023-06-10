eb33edb54857acf076b8ec763d619ec864a524f2
Yaw Sarfo <yawsarfo2019@gmail.com>
2023-06-10 15:10:33 +0000

    Added search functionality

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/home', function(req, res, next) {
+router.get('/home', async(req, res, next)=> {
+
   res.render('home', { title: 'Express' });
 });
 
-router.post('/searchFruits', async(req, res)=>{
-  var regex= new RegExp('noodles', 'i');
-  return FileModel.find({title: regex}, function(err,q){
-    return res.send(q);
-});
+router.post('/getFiles', async(req, res, next)=>{
+  let payload=req.body.payload.trim();
+  let search=await FileModel.find({title:{$regex: new RegExp('^'+ payload+'.*','i')}}).exec();
+  res.send({payload:search});
 });
 
+// preview PART IAM HERE
+// router.get('/preview/:filename', async (req, res) => {
+//   const filename = req.params.filename;
 
+//   console.log('Filename:', filename); // Log the filename to check if it's correct
+
+//   try {
+//     const file = await FileModel.findOne({ title:filename });
+
+//     if (file) {
+//       console.log('File found:', file); // Log the file object to check its contents
+
+//       res.set('Content-Type', file.fileType);
+
+//       console.log('Content-Type:', file.fileType); // Log the content type to check if it's correct
+
+//       res.send(file.data);
+//     } else {
+//       console.log('File not found'); // Log a message if the file is not found
+//       res.status(404).json({ error: 'File not found' });
+//     }
+//   } catch (error) {
+//     console.log('Error:', error); // Log any errors that occur during the database query
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
 
 module.exports = router;
